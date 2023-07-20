9a3c13994db479efcbe4b0cdbab2104bbe4152c0
Sattwik Ranjan <sattwikranjan@gmail.com>
2023-07-20 22:57:57 +0530

    Login page for admin created

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,33 @@
-
-
-import React from 'react'
-
+import React from "react";
+import { Form, Input } from "antd";
+import "../styles/loginstyles.css";
 const Login = () => {
+  //form-handler
+  const onFinishHandler = (values) => {
+    console.log(values);
+  };
   return (
-    <div>
-        <h1>Login For Admin</h1>
-    </div>
-  )
-}
+    <>
+      <div className="form-container ">
+        <Form
+          layout="vertical"
+          onFinish={onFinishHandler}
+          className="login-form"
+        >
+          <h3>Login as Admin</h3>
+          <Form.Item label="Name" name="name">
+            <Input type="text" placeholder="Enter UserName here" required />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input type="password" placeholder="Enter Password here" required />
+          </Form.Item>
+          <button className="btn btn-primary" type="submit">
+            Login as Admin
+          </button>
+        </Form>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
