00240871457fba3e2ff099e4d641a66cb8e04ba0
Sattwik Ranjan <sattwikranjan@gmail.com>
2023-07-22 23:41:23 +0530

    Redux Toolkit setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
+import { useSelector } from "react-redux";
+import Spinner from "./components/Spinner";
 
 const App = () => {
+  const { loading } = useSelector((state) => state.alerts);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 };
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Form, Input, message } from "antd";
 import "../styles/loginstyles.css";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
+
 import { useNavigate } from "react-router-dom";
 const Login = () => {
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   //form-handler
   const onFinishHandler = async (values) => {
     try {
+      dispatch(showLoading());
       const res = await axios.post("/api/v1/user/login", values);
+      dispatch(hideLoading());
       if (res.data.success) {
         localStorage.setItem("token", res.data.token);
         message.success("Login Successfully");
@@ -18,6 +23,7 @@ const Login = () => {
         message.error(res.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       console.log(error);
       message.error("Something Went Wrong");
     }
